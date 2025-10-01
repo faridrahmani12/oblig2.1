4d93b9f76beb4ee16c28a8e87191c113d3181c0a
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-01 17:40:43 +0200

    Update registrer_student.php

--- registrer_student.php
+++ registrer_student.php
@@ -1,1 +1,25 @@
+<?php
+require 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $stmt = $pdo->prepare("INSERT INTO student (brukernavn, fornavn, etternavn, klassekode) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$_POST['brukernavn'], $_POST['fornavn'], $_POST['etternavn'], $_POST['klassekode']]);
+    echo "Student registrert!";
+}
+
+$klasser = $pdo->query("SELECT * FROM klasse")->fetchAll();
+?>
+
+<form method="post">
+  Brukernavn: <input name="brukernavn"><br>
+  Fornavn: <input name="fornavn"><br>
+  Etternavn: <input name="etternavn"><br>
+  Klassekode:
+  <select name="klassekode">
+    <?php foreach ($klasser as $k) {
+      echo "<option value='{$k['klassekode']}'>{$k['klassekode']} - {$k['klassenavn']}</option>";
+    } ?>
+  </select><br>
+  <button type="submit">Lagre</button>
+</form>
 

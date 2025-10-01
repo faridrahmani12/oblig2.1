0aece0c8cc57b1195b123d1be01f571ebfb9d0ae
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-01 17:40:29 +0200

    Update registrer_klasse.php

--- registrer_klasse.php
+++ registrer_klasse.php
@@ -1,1 +1,17 @@
+<?php
+require 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $stmt = $pdo->prepare("INSERT INTO klasse (klassekode, klassenavn, studiumkode) VALUES (?, ?, ?)");
+    $stmt->execute([$_POST['klassekode'], $_POST['klassenavn'], $_POST['studiumkode']]);
+    echo "Klasse registrert!";
+}
+?>
+
+<form method="post">
+  Klassekode: <input name="klassekode"><br>
+  Klassenavn: <input name="klassenavn"><br>
+  Studiumkode: <input name="studiumkode"><br>
+  <button type="submit">Lagre</button>
+</form>
 

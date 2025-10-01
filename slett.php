b5e58a9bb85b809f5cb2611627322be61f7fe60e
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-01 17:41:35 +0200

    Update slett.php

--- slett.php
+++ slett.php
@@ -1,1 +1,15 @@
+<?php
+require 'db.php';
+
+if ($_GET['type'] === 'klasse') {
+    $stmt = $pdo->prepare("DELETE FROM klasse WHERE klassekode=?");
+    $stmt->execute([$_GET['kode']]);
+    echo "Klasse slettet!";
+}
+
+if ($_GET['type'] === 'student') {
+    $stmt = $pdo->prepare("DELETE FROM student WHERE brukernavn=?");
+    $stmt->execute([$_GET['kode']]);
+    echo "Student slettet!";
+}
 

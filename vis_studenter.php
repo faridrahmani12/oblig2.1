f4067e08178aca53d1eddafbf7fc6c57cf0ab1c9
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-01 17:41:17 +0200

    Update vis_studenter.php

--- vis_studenter.php
+++ vis_studenter.php
@@ -1,1 +1,8 @@
+<?php
+require 'db.php';
+$studenter = $pdo->query("SELECT * FROM student")->fetchAll();
+foreach ($studenter as $s) {
+    echo "{$s['brukernavn']} - {$s['fornavn']} {$s['etternavn']} ({$s['klassekode']}) 
+    <a href='slett.php?type=student&kode={$s['brukernavn']}'>Slett</a><br>";
+}
 

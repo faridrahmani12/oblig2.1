00d71a3d42d11ff13519374eda94dbdb86a51157
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-01 17:39:39 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,17 @@
+<?php
+$url = getenv("DATABASE_URL"); 
+$dbparts = parse_url($url);
+
+$host = $dbparts['host'];
+$dbname = ltrim($dbparts['path'], '/');
+$user = $dbparts['user'];
+$pass = $dbparts['pass'];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
 

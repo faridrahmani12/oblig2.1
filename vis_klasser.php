37ba726ae2c52c8ed30cab11fe643fc18bf18104
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-01 17:41:00 +0200

    Update vis_klasser.php

--- vis_klasser.php
+++ vis_klasser.php
@@ -1,1 +1,8 @@
+<?php
+require 'db.php';
+$klasser = $pdo->query("SELECT * FROM klasse")->fetchAll();
+foreach ($klasser as $k) {
+    echo "{$k['klassekode']} - {$k['klassenavn']} ({$k['studiumkode']}) 
+    <a href='slett.php?type=klasse&kode={$k['klassekode']}'>Slett</a><br>";
+}
 

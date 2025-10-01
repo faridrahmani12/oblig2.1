1b3e90e9b09b29c18ab1e9b12365c9a96aaed310
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-01 17:40:15 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,1 +1,24 @@
+<?php
+require 'db.php';
+
+$sql = "
+CREATE TABLE IF NOT EXISTS klasse (
+  klassekode CHAR(5) NOT NULL,
+  klassenavn VARCHAR(50) NOT NULL,
+  studiumkode VARCHAR(50) NOT NULL,
+  PRIMARY KEY (klassekode)
+);
+
+CREATE TABLE IF NOT EXISTS student (
+  brukernavn CHAR(7) NOT NULL,
+  fornavn VARCHAR(50) NOT NULL,
+  etternavn VARCHAR(50) NOT NULL,
+  klassekode CHAR(5) NOT NULL,
+  PRIMARY KEY (brukernavn),
+  FOREIGN KEY (klassekode) REFERENCES klasse(klassekode)
+);
+";
+
+$pdo->exec($sql);
+echo "Tabellene er opprettet!";
 

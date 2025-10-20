047f2aae8dcb8542f541ad87a5472f96303139bb
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-20 14:39:28 +0200

    Add language and charset attributes to HTML

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 
 <!DOCTYPE html>
-<html>
-<head><title>Meny</title></head>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title>Meny</title>
+</head>
 <body>
-<h1>Velg funksjon</h1>
-<ul>
-  <li><a href="registrer_klasse.php">Registrer klasse</a></li>
-  <li><a href="registrer_student.php">Registrer student</a></li>
-  <li><a href="vis_klasser.php">Vis alle klasser</a></li>
-  <li><a href="vis_studenter.php">Vis alle studenter</a></li>
-</ul>
+    <h1>Velg funksjon</h1>
+    <ul>
+        <li><a href="registrer_klasse.php">Registrer klasse</a></li>
+        <li><a href="registrer_student.php">Registrer student</a></li>
+        <li><a href="vis_klasser.php">Vis alle klasser</a></li>
+        <li><a href="vis_studenter.php">Vis alle studenter</a></li>
+    </ul>
 </body>
 </html>

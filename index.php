be93cf1214824b5cfeb9ba4ddd146227c04e96fd
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-20 18:20:28 +0200

    Refactor HTML structure in index.php

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <!DOCTYPE html>
 <html lang="no">
 <head>
-  <meta charset="UTF-8" />
-  <title>Meny</title>
-  <style>
-    body{font-family:system-ui,Arial,sans-serif;max-width:720px;margin:40px auto}
-    a{text-decoration:none;color:#4527a0}
-    ul{line-height:2}
-    h1{font-size:3rem;margin-bottom:1rem}
-  </style>
+    <meta charset="UTF-8">
+    <title>Meny</title>
 </head>
 <body>
-  <h1>Meny</h1>
-  <ul>
-    <li><a href="vis_klasser.php">Vis alle klasser</a></li>
-    <li><a href="vis_studenter.php">Vis alle studenter</a></li>
-    <li><a href="registrer_klasse.php">Registrer klasse</a></li>
-    <li><a href="registrer_student.php">Registrer student</a></li>
-    <li><a href="setup.php">Kjør setup (oppretter tabeller + demodata)</a></li>
-  </ul>
+    <h1>Meny</h1>
+    <ul>
+        <li><a href="vis_klasser.php">Vis alle klasser</a></li>
+        <li><a href="vis_studenter.php">Vis alle studenter</a></li>
+        <li><a href="registrer_klasse.php">Registrer klasse</a></li>
+        <li><a href="registrer_student.php">Registrer student</a></li>
+        <li><a href="setup.php">Kjør setup (oppretter tabeller + demodata)</a></li>
+    </ul>
 </body>
 </html>

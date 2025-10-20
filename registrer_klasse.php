d8f9b593268f9a6eb4739a777e8debf44349ed9f
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-20 14:40:21 +0200

    Make form fields required and update success message
    Updated form inputs to be required and modified success message formatting.

--- registrer_klasse.php
+++ registrer_klasse.php
@@ -4,14 +4,13 @@ require 'db.php';
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $stmt = $pdo->prepare("INSERT INTO klasse (klassekode, klassenavn, studiumkode) VALUES (?, ?, ?)");
     $stmt->execute([$_POST['klassekode'], $_POST['klassenavn'], $_POST['studiumkode']]);
-    echo "Klasse registrert!";
+    echo "<p>Klasse registrert!</p>";
 }
 ?>
-
 <form method="post">
-  Klassekode: <input name="klassekode"><br>
-  Klassenavn: <input name="klassenavn"><br>
-  Studiumkode: <input name="studiumkode"><br>
-  <button type="submit">Lagre</button>
+    Klassekode: <input name="klassekode" required><br>
+    Klassenavn: <input name="klassenavn" required><br>
+    Studiumkode: <input name="studiumkode" required><br>
+    <button type="submit">Lagre</button>
 </form>
-
+<p><a href="index.php">Tilbake til meny</a></p>

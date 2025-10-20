60d93f5cc299dea70a0b667a42f3a0025dc610f8
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-20 14:42:55 +0200

    Refactor class display with a table format

--- vis_klasser.php
+++ vis_klasser.php
@@ -1,8 +1,18 @@
 <?php
 require 'db.php';
-$klasser = $pdo->query("SELECT * FROM klasse")->fetchAll();
-foreach ($klasser as $k) {
-    echo "{$k['klassekode']} - {$k['klassenavn']} ({$k['studiumkode']}) 
-    <a href='slett.php?type=klasse&kode={$k['klassekode']}'>Slett</a><br>";
-}
+$klasser = $pdo->query("SELECT * FROM klasse ORDER BY klassekode")->fetchAll();
+?>
+<h2>Alle klasser</h2>
+<table border="1" cellpadding="5">
+<tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th><th>Handling</th></tr>
+<?php foreach ($klasser as $k): ?>
+<tr>
+  <td><?= htmlspecialchars($k['klassekode']) ?></td>
+  <td><?= htmlspecialchars($k['klassenavn']) ?></td>
+  <td><?= htmlspecialchars($k['studiumkode']) ?></td>
+  <td><a href="slett.php?type=klasse&kode=<?= urlencode($k['klassekode']) ?>">Slett</a></td>
+</tr>
+<?php endforeach; ?>
+</table>
+<p><a href="index.php">Tilbake til meny</a></p>
 

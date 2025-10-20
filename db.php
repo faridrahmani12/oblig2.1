f1d810f9c5080105073c7eb0566b3dec3a10431a
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-20 15:30:32 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,9 @@
 <?php
-$host = "mysql";               // USN sin database-server
-$dbname = "farah6535";         // Ditt databasenavn
-$user = "farah6535";           // Ditt brukernavn
-$pass = "a999farah6535";       // Ditt passord
+// USN Dokploy: bruk verdiene dine her
+$host = "mysql";
+$dbname = "farah6535";
+$user = "farah6535";
+$pass = "a999farah6535";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);

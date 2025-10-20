e974173597610eb4910841ac2cad6bf7b73ab656
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-20 18:20:05 +0200

    Update database connection credentials

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'mysql';
-$user = 'farah6535';
-$pass = 'a999farah6535';
-$dbname = 'farah6535';
+$host = "mysql-farah6535.dokploy.usn.no";
+$user = "farah6535";
+$pass = "a999farah6535";
+$dbname = "farah6535";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 

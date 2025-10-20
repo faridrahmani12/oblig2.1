4abe957d1a0d5c6f6a7cc7ababe640b36bb4849d
faridrahmani12 <71428587+faridrahmani12@users.noreply.github.com>
2025-10-20 14:43:23 +0200

    Refactor student display with HTML table format

--- vis_studenter.php
+++ vis_studenter.php
@@ -1,8 +1,18 @@
 <?php
 require 'db.php';
-$studenter = $pdo->query("SELECT * FROM student")->fetchAll();
-foreach ($studenter as $s) {
-    echo "{$s['brukernavn']} - {$s['fornavn']} {$s['etternavn']} ({$s['klassekode']}) 
-    <a href='slett.php?type=student&kode={$s['brukernavn']}'>Slett</a><br>";
-}
-
+$studenter = $pdo->query("SELECT * FROM student ORDER BY brukernavn")->fetchAll();
+?>
+<h2>Alle studenter</h2>
+<table border="1" cellpadding="5">
+<tr><th>Brukernavn</th><th>Fornavn</th><th>Etternavn</th><th>Klassekode</th><th>Handling</th></tr>
+<?php foreach ($studenter as $s): ?>
+<tr>
+  <td><?= htmlspecialchars($s['brukernavn']) ?></td>
+  <td><?= htmlspecialchars($s['fornavn']) ?></td>
+  <td><?= htmlspecialchars($s['etternavn']) ?></td>
+  <td><?= htmlspecialchars($s['klassekode']) ?></td>
+  <td><a href="slett.php?type=student&kode=<?= urlencode($s['brukernavn']) ?>">Slett</a></td>
+</tr>
+<?php endforeach; ?>
+</table>
+<p><a href="index.php">Tilbake til meny</a></p>
